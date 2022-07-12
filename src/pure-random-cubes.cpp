05eafa98a603348f5c15e3abf2157049de37631b
Abhishek Sharma <avisekksarma@gmail.com>
2022-07-12 11:27:02 +0545

    need to see camera part again as this is incomplete

--- src/pure-random-cubes.cpp
+++ src/pure-random-cubes.cpp
@@ -13,14 +13,25 @@
 #include "../include/stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void processInput(GLFWwindow *window,float & valx,float &valy,float & valz);
 
+void processInput(GLFWwindow *window, glm::vec3 &cameraPos, glm::vec3 &cameraFront, glm::vec3 &cameraUp);
 // to see this lesson specific - search for 3d-part zone
 #define cubes_no 10
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// deltatime related thing
+float currentFrame;
+bool firstMouse = true;
+float yaw = -90.0f; // yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 45.0f;
+
+float deltaTime = 0.0f; // time between current frame and last frame
+float lastFrame = 0.0f;
 int main()
 {
     // Use current time as seed for random generator
@@ -225,25 +236,28 @@ int main()
     for (int i = 0; i < cubes_no; i++)
     {
         float x = (rand() % 11) / 10.0f;
-        int sign = (rand()%2)?-1:1;
+        int sign = (rand() % 2) ? -1 : 1;
         x = x * sign;
         float y = (rand() % 11) / 10.0f;
-        sign = (rand()%2)?-1:1;
+        sign = (rand() % 2) ? -1 : 1;
         y = y * sign;
         float z = (rand() % 11) / 10.0f;
-        sign = (rand()%2)?-1:1;
+        sign = (rand() % 2) ? -1 : 1;
         z = z * sign;
-        glm::vec3 a = glm::vec3(x,y,z);
+        glm::vec3 a = glm::vec3(x, y, z);
         axis.push_back(a);
     }
-    float valx,valy,valz;
-    valx=valy=valz=1.0f;
-
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
     while (!glfwWindowShouldClose(window))
     {
         // input
+        currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
         // -----
-        processInput(window,valx,valy,valz);
+        processInput(window, cameraPos, cameraFront, cameraUp);
 
         // render
         // ------
@@ -255,10 +269,12 @@ int main()
         // create transformations
         glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
         glm::mat4 view = glm::mat4(1.0f);
+
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         glm::mat4 projection = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(valx, valy, valz));
+        // view = glm::translate(view, glm::vec3(valx, valy, valz));
         // VVI: based on playing with perspective projection: i found that
-        // in aspect ratio if width>height then height is increased of the box in view space 
+        // in aspect ratio if width>height then height is increased of the box in view space
         // and vice versa
         projection = glm::perspective(glm::radians(55.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f); // draw our first triangle
         unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
@@ -296,22 +312,19 @@ int main()
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window,float & valx,float &valy,float & valz)
+void processInput(GLFWwindow *window, glm::vec3 &cameraPos, glm::vec3 &cameraFront, glm::vec3 &cameraUp)
 {
+    const float cameraSpeed = 2.5f * deltaTime; // adjust accordingly
     if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-    else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-        valx -= 0.1f;
-    else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-        valx += 0.1f;
-    else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-        valy -= 0.1f;
-    else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-        valy += 0.1f;
-    else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        valz -= 0.1f;
-    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        valz += 0.1f;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::vec3(0.07f, 0.0f, 0.0f);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += (glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -321,4 +334,36 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
     // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+void mouse_callback(GLFWwindow *window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
 }
